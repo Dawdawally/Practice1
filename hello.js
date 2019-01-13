081d2ea41e761bff92cd21293b4ba544b8473de3
Dawda Wally <wallydawda@gmail.com>
2019-01-13 08:52:20 +0100

    hello

---
+++ hello.js
@@ -0,0 +1,32 @@
+var xhttp = new HMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        document.getElementById("hello").innerHTML = this.responseText;
+    }
+    xhhtp.open("Get", "dawda.js", true);
+    xhttp.send();
+}
+
+var person = {
+    name: "Dawda",
+    surname:"Wally",
+
+    fullName = function(){
+        return this.name + "" + this.surname;
+    }  
+}
+
+var person1 = {
+    name: "Dawda",
+    surname: "Wallyyyyy",
+
+    get disc(){
+        return this.name;
+    },
+    set disc(fullname){
+        this.Fullname = fullname;
+    }
+};
+
+person.fullName.call(person1);
+person1.prototype.Nationality = "Gambian";
